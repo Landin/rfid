ad9997ece6d9ba362f150952a47d2f00bf4b7682
kristofferl <landinius84@gmail.com>
2014-09-21 15:00:14 +0200

    enter directive

---
+++ packages/rfid/public/directives/enter.js
@@ -0,0 +1,14 @@
+'use strict';
+angular.module('mean.rfid').directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
