a1678dda974b3eaba6c846b2e8bb76c6eeba8072
kristofferl <landinius84@gmail.com>
2014-09-21 16:32:42 +0200

    rensat skräp

--- packages/rfid/public/services/card.js
+++ packages/rfid/public/services/card.js
@@ -12,16 +12,3 @@ angular.module('mean.rfid').factory('Cards', ['$resource',
     });
   }
 ]);
-
-////Articles service used for articles REST endpoint
-//angular.module('mean.articles').factory('Articles', ['$resource',
-//  function($resource) {
-//    return $resource('articles/:articleId', {
-//      articleId: '@_id'
-//    }, {
-//      update: {
-//        method: 'PUT'
-//      }
-//    });
-//  }
-//]);
